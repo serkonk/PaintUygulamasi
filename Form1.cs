bbc7246788fa0fc29187edf8a4abe3313062fd80
serkonk <serkankasik00@gmail.com>
2023-08-31 10:58:19 +0300

    Paint
    Farklı renk seçenekleriiyle ekrana çizim yapabilen paint uygulaması

---
+++ Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyPaintApp
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bmp = new Bitmap(1024, 768);
+        Pen p = new Pen(Color.Black, 5);
+        bool drawing = false; 
+        
+        public Form1()
+        {
+            
+
+            InitializeComponent();
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_MouseClick(object sender, EventArgs e)
+        {
+            if (drawing)
+                drawing = false;
+            else
+                drawing = true;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(drawing)
+            {
+                Graphics g = Graphics.FromImage(bmp);
+                g.DrawEllipse(p, e.X, e.Y, 3, 1);
+                pictureBox1.Image = bmp;
+}
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            p.Color = Color.Black;
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            p.Color = Color.Pink;
+        }
+
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+            p.Color = Color.Red;
+        }
+
+        private void toolStripButton4_Click(object sender, EventArgs e)
+        {
+            p.Color = Color.Blue;
+
+        }
+
+        private void toolStripButton5_Click(object sender, EventArgs e)
+        {
+            p.Color = Color.Green;
+
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog1.Filter = "Jpeg Image|* .jpg";
+
+
+            saveFileDialog1.Title = "Dosyayı Kaydedin";
+            saveFileDialog1.ShowDialog();
+
+
+            if (saveFileDialog1.FileName!="")
+            {
+                System.IO.FileStream fs = (System.IO.FileStream)saveFileDialog1.OpenFile();
+                switch(saveFileDialog1.FilterIndex)
+                {
+                    case 1:
+                        this.pictureBox1.Image.Save(fs, System.Drawing.Imaging.ImageFormat.Jpeg);
+                        break;
+                }
+            }
+        }   
+    }
+}
